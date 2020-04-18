b8edf3b74f7adb9e9282cfd6289db1abf27caebc
Vincent Tran <vihutr@gmail.com>
2020-04-17 21:41:45 -0700

    hp bar + start log

--- source/engine.py
+++ source/engine.py
@@ -4,6 +4,7 @@ from components.fighter import Fighter
 from death_functions import kill_monster, kill_player
 from entity import Entity, get_blocking_entities_at_location
 from fov_functions import initialize_fov, recompute_fov
+from game_messages import MessageLog
 from game_states import GameStates
 from input_handlers import handle_keys
 from map_objects.game_map import GameMap
@@ -15,8 +16,16 @@ def main():
         screen_width = 80
         screen_height = 50
 
+        bar_width = 20
+        panel_height = 7
+        panel_y = screen_height-panel_height
+        
+        message_x = bar_width + 2
+        message_width = screen_width - bar_width - 2
+        message_height = panel_height - 1
+
         map_width = 80
-        map_height = 45
+        map_height = 43
 
         room_max_size = 10
         room_min_size = 6
@@ -46,12 +55,15 @@ def main():
         libtcod.console_init_root(screen_width, screen_height, game_title, False)
 
         con = libtcod.console_new(screen_width, screen_height)
+        panel = libtcod.console_new(screen_width, panel_height)
 
         game_map = GameMap(map_width, map_height)
         game_map.make_map(max_rooms, room_min_size, room_max_size, map_width, map_height, player, entities, max_monsters_per_room)
 
         fov_recompute = True
         fov_map = initialize_fov(game_map)
+
+        message_log = MessageLog(message_x, message_width, message_height)
 
         key = libtcod.Key()
         mouse = libtcod.Mouse()
@@ -64,7 +76,8 @@ def main():
                 if fov_recompute:
                         recompute_fov(fov_map, player.x, player.y, fov_radius, fov_light_walls, fov_algorithm)
 
-                render_all(con, entities, player, game_map, fov_map, fov_recompute, screen_width, screen_height, colors)
+                render_all(con, panel, entities, player, game_map, fov_map, fov_recompute, screen_width, screen_height,
+                                bar_width, panel_height, panel_y, colors)
                 libtcod.console_flush()
 
                 clear_all(con, entities)
